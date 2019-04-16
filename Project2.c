d10dd629133d710d78d511a004848235d80cbc13
Pranjal Raj <32640067+Pranjalraj0046@users.noreply.github.com>
2019-04-16 23:38:51 +0530

    Update Project2.c

--- Project2.c
+++ Project2.c
@@ -1,22 +1,16 @@
-#include<stdlib.h>
 #include<stdio.h>
-#include<conio.h>
-int p_request=145;
-int c_request=143;
-int Find(int v)
+#include<stdlib.h>
+#include<stdbool.h>
+
+struct cylinder
 {
-	if(v>0)
-	{
-		return v;
-	}
-	else
-	{
-		return (v*-1);
-	}
-}
+	int r_no;
+	bool kt;
+};
+
 int main()
 {
-	printf("\t\t\t\t\t************************************************************************************************\n");
+	printf("\n\n\t\t\t\t\t************************************************************************************************\n");
 	printf("\t\t\t\t\t************************************************************************************************\n");
 	printf("\t\t\t\t\t***\t\t\t\t\t\t\t\t\t\t                     ***\n");
 	printf("\t\t\t\t\t***\t\t\t\t\t\t\t\t\t\t                     ***\n");
@@ -25,68 +19,98 @@ int main()
 	printf("\t\t\t\t\t***\t\t\t\t\t\t\t\t\t\t                     ***\n");
 	printf("\t\t\t\t\t************************************************************************************************\n");
 	printf("\t\t\t\t\t************************************************************************************************\n\n\n\n");
-	int queue[] = {86, 1470, 913, 1774, 948, 1509, 1022, 1750, 130};
-	int m,Begin,l=sizeof(queue)/sizeof(int),d=0,End,initial,pt,n;
-	 for (m = 0; m < l; ++m) 
-        {
-            for (n = m + 1; n < l; ++n)
-            {
-                if (queue[m] > queue[n]) 
-                {
-                    pt =  queue[m];
-                    queue[m] = queue[n];
-                    queue[n] = pt;
-                }
-            } 
-        }
-        End = c_request;
-    if(c_request>=p_request){
-        for(m=0; m<l; m++){
-            if(queue[m]>c_request){
-                Begin=m;
-                break;
-            }
-        }
-        printf("Order    : %4d",End);
-        for(m=Begin; m<l; m++){
-            printf(", %4d",queue[m]);
-             for(m=Begin; m<l; m++)
-        for(m=Begin; m>0; m--){
-            printf(", %4d",queue[m-1]);
-            initial = queue[m];
+	
+	int m,n_request,current_h,limit,n,flag,p_head;
+	printf("Enter the head requests number: ");
+	scanf("%d",&n_request);
+	struct cylinder a[n_request];
+	
+	for (m = 0; m < n_request; ++m)
+	{
+		printf("Enter the %d requests: ",m+1);
+		scanf("%d",&a[m].r_no);
+		a[m].kt = false;
+	}
+	printf("\nEnter the current request of head: ");
+	scanf("%d",&current_h);
 
-            d+= Find(End-initial);
-            printf("[%d]",Find(End-initial) );
-            End = initial;
-        }
-}
-}
-      else if(c_request<p_request){
-        for(m=0; m<l; m++){
-            if(queue[m]>c_request){
-                Begin=m;
-                break;
-            }
-        }
-        printf("Sequence : %4d",End);
-        for(m=Begin-1; m>=0; m--){
-            printf(", %4d",queue[m]);
-            initial = queue[m];
+	printf("\nEnter the previous request of head: ");
+	scanf("%d",&p_head);
 
-            d+= Find(End-initial);
-            printf("[%d]",Find(End-initial) );
-            End = initial;
-        }
-        for(m=Begin; m<l; m++){
-            printf(", %4d",queue[m]);
-            initial = queue[m];
-
-            d+= Find(End-initial);
-            printf("[%d]",Find(End-initial) );
-            End = initial;
-        }
-    }
-    printf("\n\n\nTotal Distance : %d\n",d);
-    return 0;
-    getch();
+	printf("\nEnter the maximum size cylinder: ");
+	scanf("%d",&limit);
+	
+	printf("\nSequence of Head Movement\n\n");
+	if(p_head - current_h > 0 )
+	{
+		flag = 2;
+	}
+	else
+		flag = 1;
+	int distance=0;
+	printf("%d -> ",current_h );
+	int i_head = current_h;
+	if(flag == 1)
+	{
+		for(m=current_h;m<limit;m++)
+		{
+			for(n=0;n<n_request;n++)
+			{
+				if(a[n].r_no == m && a[n].kt == false)
+				{
+					printf("%d -> ", a[n].r_no);
+					a[n].kt = true;
+					distance += abs(a[n].r_no - current_h);
+					current_h = a[n].r_no;
+				}
+			}
+		}
+		for(m=0;m<i_head;m++)
+		{
+			for(n=0;n<n_request;n++)
+			{
+				if(a[n].r_no == m && a[n].kt == false)
+				{
+					printf("%d -> ", a[n].r_no);
+					a[n].kt = true;
+					distance += abs(a[n].r_no - current_h);
+					current_h = a[n].r_no;
+				}
+			}
+		}
+		printf("\n");
+	}
+	else if(flag == 2)
+	{
+		for(m=current_h;m>=0;m--)
+		{
+			for(n=0;n<n_request;n++)
+			{
+				if(a[n].r_no == m && a[n].kt == false)
+				{
+					printf("%d -> ", a[n].r_no);
+					a[n].kt = true;
+					distance += abs(a[n].r_no - current_h);
+					current_h = a[n].r_no;
+				}
+			}
+		}
+		current_h = limit-1;
+		for(m=limit;m>i_head;m--)
+		{
+			for(n=0;n<n_request;n++)
+			{
+				if(a[n].r_no == m && a[n].kt == false)
+				{
+					printf("%d -> ", a[n].r_no);
+					a[n].kt = true;
+					distance += abs(a[n].r_no - current_h);
+					current_h = a[n].r_no;
+				}
+			}
+		}
+		printf("\n");
+	}
+	printf("\nTotal Distance: %d\n", distance);
 }
+
